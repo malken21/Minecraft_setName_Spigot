471f870e242a59aa4396b1794d754cea29c0895b
malken21 <Malken_Marumasa@outlook.jp>
2022-09-12 01:47:19 +0900

    Add files via upload

---
+++ src/main/java/marumasa/set_name/Config.java
@@ -0,0 +1,25 @@
+package marumasa.set_name;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class Config {
+
+    public final String name_NoItemText;
+    public final String name_ResultText;
+    public final String lore_NoItemText;
+    public final String lore_ResultText;
+    public final String colorcode;
+
+    public Config(minecraft plugin) {
+        plugin.saveDefaultConfig();
+        FileConfiguration config = plugin.getConfig();
+
+        name_NoItemText = config.getString("setname.NoItemText");
+        name_ResultText = config.getString("setname.ResultText");
+
+        lore_NoItemText = config.getString("setlore.NoItemText");
+        lore_ResultText = config.getString("setlore.ResultText");
+
+        colorcode = config.getString("colorcode");
+    }
+}
